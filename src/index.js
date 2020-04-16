6a76b54fef87735198abcb685b0e83a9e412f78b
Julie Berteaux <julie.berteaux96@gmail.com>
2020-04-16 19:34:06 +0200

    use bulma dependencies

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+
+import 'bulma/css/bulma.css';
 import 'mdbreact/dist/css/mdb.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
